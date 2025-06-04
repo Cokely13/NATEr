2f2caa1ec733f71c641c3e373a4baa8f13f446c7
Ryan Cokely <ryan.cokely@gmail.com>
2025-06-03 21:36:45 -0400

    fixing

--- lib/withUser.ts
+++ lib/withUser.ts
@@ -1,39 +1,3 @@
-// // lib/withUser.ts
-// import { GetServerSidePropsContext } from "next";
-// import { getIronSession } from "iron-session";
-// import { sessionOptions } from "./session";
-// import "../types/ironSessionTypes";
-
-// export function requireUser(
-//   gssp: (ctx: GetServerSidePropsContext, user: any) => Promise<{ props: any }>
-// ) {
-//   return async function handler(ctx: GetServerSidePropsContext) {
-//     const req = ctx.req;
-//     const res = ctx.res;
-
-//     const session = await getIronSession(req, res, sessionOptions);
-//     const user = session.user;
-
-//     if (!user) {
-//       return {
-//         redirect: {
-//           destination: "/login",
-//           permanent: false,
-//         },
-//       };
-//     }
-
-//     const gsspData = await gssp(ctx, user);
-
-//     return {
-//       props: {
-//         user,
-//         ...("props" in gsspData ? gsspData.props : {}),
-//       },
-//     };
-//   };
-// }
-
 import { GetServerSidePropsContext } from "next";
 import { getIronSession, IronSession } from "iron-session";
 import { sessionOptions } from "./session";
--- src/pages/api/auth/getUser.ts
+++ src/pages/api/auth/getUser.ts
@@ -1,5 +1,21 @@
+// // src/pages/api/auth/getUser.ts
+// import { getIronSession } from "iron-session";
+// import { sessionOptions } from "../../../../lib/session";
+// import { NextApiRequest, NextApiResponse } from "next";
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const session = await getIronSession(req, res, sessionOptions);
+//   if (!session.user) {
+//     return res.status(401).json({ user: null });
+//   }
+//   res.status(200).json({ user: session.user });
+// }
+
 // src/pages/api/auth/getUser.ts
-import { getIronSession } from "iron-session";
+import { getIronSession, IronSessionData } from "iron-session";
 import { sessionOptions } from "../../../../lib/session";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -7,9 +23,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await getIronSession(req, res, sessionOptions);
+  const session = (await getIronSession(
+    req,
+    res,
+    sessionOptions
+  )) as IronSessionData;
+
   if (!session.user) {
     return res.status(401).json({ user: null });
   }
+
   res.status(200).json({ user: session.user });
 }
