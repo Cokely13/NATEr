983bab09aeb9983ab25b16383e885cf4db949818
Ryan Cokely <ryan.cokely@gmail.com>
2025-06-03 20:46:43 -0400

    chop

--- lib/withUser.ts
+++ lib/withUser.ts
@@ -1,17 +1,68 @@
-// lib/withUser.ts
+// // lib/withUser.ts
+// import { GetServerSidePropsContext } from "next";
+// import { getIronSession } from "iron-session";
+// import { sessionOptions } from "./session";
+// import "../types/ironSessionTypes";
+
+// export function requireUser(
+//   gssp: (ctx: GetServerSidePropsContext, user: any) => Promise<{ props: any }>
+// ) {
+//   return async function handler(ctx: GetServerSidePropsContext) {
+//     const req = ctx.req;
+//     const res = ctx.res;
+
+//     const session = await getIronSession(req, res, sessionOptions);
+//     const user = session.user;
+
+//     if (!user) {
+//       return {
+//         redirect: {
+//           destination: "/login",
+//           permanent: false,
+//         },
+//       };
+//     }
+
+//     const gsspData = await gssp(ctx, user);
+
+//     return {
+//       props: {
+//         user,
+//         ...("props" in gsspData ? gsspData.props : {}),
+//       },
+//     };
+//   };
+// }
+
 import { GetServerSidePropsContext } from "next";
-import { getIronSession } from "iron-session";
+import { getIronSession, IronSession } from "iron-session";
 import { sessionOptions } from "./session";
-import "../types/ironSessionTypes";
+
+interface SessionUser {
+  id: number;
+  name: string;
+  email: string;
+}
+
+interface SessionData extends IronSession {
+  user?: SessionUser;
+}
 
 export function requireUser(
-  gssp: (ctx: GetServerSidePropsContext, user: any) => Promise<{ props: any }>
+  gssp: (
+    ctx: GetServerSidePropsContext,
+    user: SessionUser
+  ) => Promise<{ props: any }>
 ) {
   return async function handler(ctx: GetServerSidePropsContext) {
     const req = ctx.req;
     const res = ctx.res;
 
-    const session = await getIronSession(req, res, sessionOptions);
+    const session = (await getIronSession(
+      req,
+      res,
+      sessionOptions
+    )) as SessionData;
     const user = session.user;
 
     if (!user) {
