015c8cd293df87634c9f1530cbe3635a43aac841
Ryan Cokely <ryan.cokely@gmail.com>
2025-06-03 21:38:36 -0400

    add types to login

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,9 +1,46 @@
+// import { getIronSession } from "iron-session";
+// import { sessionOptions } from "../../../../lib/session";
+// import { prisma } from "../../../../prisma/prisma";
+// import bcrypt from "bcrypt";
+
+// export default async function handler(req, res) {
+//   if (req.method !== "POST") return res.status(405).end();
+
+//   const { name, password } = req.body;
+
+//   const user = await prisma.user.findFirst({ where: { name } });
+
+//   if (!user || !user.password) {
+//     return res.status(401).json({ message: "Invalid credentials" });
+//   }
+
+//   const isValid = await bcrypt.compare(password, user.password);
+//   if (!isValid) {
+//     return res.status(401).json({ message: "Invalid credentials" });
+//   }
+
+//   const session = await getIronSession(req, res, sessionOptions);
+//   session.user = { id: user.id, name: user.name, email: user.email };
+//   await session.save();
+
+//   res
+//     .status(200)
+//     .json({
+//       message: "Logged in",
+//       user: { id: user.id, name: user.name, email: user.email },
+//     });
+// }
+
 import { getIronSession } from "iron-session";
 import { sessionOptions } from "../../../../lib/session";
 import { prisma } from "../../../../prisma/prisma";
 import bcrypt from "bcrypt";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "POST") return res.status(405).end();
 
   const { name, password } = req.body;
@@ -23,10 +60,8 @@ export default async function handler(req, res) {
   session.user = { id: user.id, name: user.name, email: user.email };
   await session.save();
 
-  res
-    .status(200)
-    .json({
-      message: "Logged in",
-      user: { id: user.id, name: user.name, email: user.email },
-    });
+  res.status(200).json({
+    message: "Logged in",
+    user: { id: user.id, name: user.name, email: user.email },
+  });
 }
